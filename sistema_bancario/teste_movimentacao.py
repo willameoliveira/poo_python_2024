a2441d317774850a4bce54d1a0a934240bc92771
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2025-01-03 10:04:05 -0300

    Adicionando movimentacoes da conta

--- sistema_bancario/teste_movimentacao.py
+++ sistema_bancario/teste_movimentacao.py
@@ -1,4 +1,12 @@
 from movimentacao import Movimentacao
+from conta import Conta
+from cliente import Cliente
 
 movimentacao1 = Movimentacao(nome="Saque", valor=100)
-print(f"{movimentacao1.data} - {movimentacao1.nome} - R$ {movimentacao1.valor}")
+print(movimentacao1)
+
+cliente1 = Cliente(nome="José", cpf="233", endereco="Rua X", telefone="12", data_nasc=None)
+conta1 = Conta(numero=12, saldo=200, cliente=cliente1)
+
+movimentacao2 = Movimentacao(nome="Transferência", valor=100, conta_destino=conta1)
+print(movimentacao2)
