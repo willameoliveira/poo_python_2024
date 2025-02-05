e063be87fe156a3c4e63df97034bd69f1e34c8d7
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2025-02-05 16:42:31 -0300

    Sistema de venda 2.2

--- sistema_venda_v2/teste_venda.py
+++ sistema_venda_v2/teste_venda.py
@@ -1,11 +1,17 @@
 from produto import Produto
 from venda import Venda
+import os
 
-produto1 = Produto("Mouse sem fio", 49.90, 30)
+os.system("clear")
+produto1 = Produto("Mouse sem fio", 50, 30)
 
-venda1 = Venda(produto=produto1, 
-               quantidade_vendida=10, 
-               forma_pagamento="Cartão de crédito")
+venda1 = Venda(forma_pagamento="Cartão de crédito")
+venda1.adicionar_item(produto=produto1, quantidade=2)
+
+produto2 = Produto("Teclado", 15, 30)
+
+venda1.adicionar_item(produto=produto2, quantidade=1)
 
 print(venda1) #chamando o método __str__ implicitamente
+
 print(f"Troco (valor pago 500): R$ {venda1.calcular_troco(500)}")
--- sistema_venda_v2/venda.py
+++ sistema_venda_v2/venda.py
@@ -1,30 +1,41 @@
 from datetime import datetime
+from item_venda import Item_Venda
 from produto import Produto
 
 class Venda:
 
-    def __init__(self, produto: Produto, quantidade_vendida: int, forma_pagamento: str, data_venda:datetime = datetime.now()):
-        self.produto = produto
-        self.valor_total = 0
-        self.quantidade_vendida = quantidade_vendida
-        self.forma_pagamento = forma_pagamento
-        self.data_venda = data_venda
+    def __init__(self, forma_pagamento: str, data_venda:datetime = datetime.now()):
+        self._valor_total = 0
+        self._forma_pagamento = forma_pagamento
+        self._data_venda = data_venda
+        self._itens = []
     
+    @property
+    def itens(self) -> list[Item_Venda]:
+        return self._itens
+    
+    def adicionar_item(self, produto: Produto, quantidade: int):
+        self._itens.append(Item_Venda(produto, quantidade))
+
     def calcular_valor_total(self) -> float:
-        self.valor_total = self.produto.valor * self.quantidade_vendida
-        return self.valor_total
+        self._valor_total = sum(item.valor for item in self._itens)
+        return self._valor_total
 
     def calcular_troco(self, valorPago: float) -> float:
-        return valorPago - self.calcular_valor_total()
+        if self._valor_total > 0 and valorPago > 0:
+            return valorPago - self._valor_total
+        return 0.0
 
     def __str__(self):
-        return (f"""
-                Venda realizada em: {self.data_venda.strftime("%d/%m/%y %H:%M")} 
-                Produto: {self.produto.descricao}
-                Valor unitário: {self.produto.valor}
-                Quantidade de itens: {self.quantidade_vendida}
-                Valor total: R${self.calcular_valor_total()}
-                Forma de pagamento: {self.forma_pagamento}""")
+        itens = ""
+        for item in self.itens:
+            itens += f"{item.produto.descricao}\t{item.quantidade}\tR$ {item.valor}\n"
+
+        string = f"Venda realizada em: {self._data_venda.strftime('%d/%m/%y %H:%M')}\n" 
+        string += itens
+        string += f"Valor total: R$ {self.calcular_valor_total()}\n"
+        string += f"Forma de pagamento: {self._forma_pagamento}"
+        return string
     
 
 
